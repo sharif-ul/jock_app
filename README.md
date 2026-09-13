81bb2373e0250f22c5eaa4dd6ba697f4d374cec3
shariful <sh7arif@gmail.com>
2026-09-13 18:35:41 +0200

    Initial Kubernetes deployment

---
+++ README.md
@@ -0,0 +1,1 @@
+# jock_app
---
+++ app.py
@@ -0,0 +1,30 @@
+# app.py
+import random
+import socket
+from flask import Flask
+
+app = Flask(__name__)
+
+# A simple list of compleely neutral jokes.
+
+JOKES = [
+    "What has an eye, but cannot see? A needle.",
+    "What kind of tree fits in your hand? A palm tree.",
+    "What gets wet while drying? A towel.",
+    "What can't a bicycl stand on its own? Because it is two-tired.",
+    "What has a head and a tail, but no body? A coin.",
+    "What has hands, but cannot clap? A clock.",
+    "What has a neck, but no head? A bottle.",
+    "What has a thumb and four fingers, but is not alive? A glove.",
+    ]
+
+@app.route("/")
+def tell_a_joke():
+    joke = random.choice(JOKES)
+    hostname = socket.gethostname()
+    # Add a V2 marker to the output!
+    return f"{joke}\n[V2] (Served by : {hostname}\n)"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
+
